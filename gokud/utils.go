02285cba89bad2037781751f8c5e5a0ff1969824
Khash Sajadi <khash@cloud66.com>
2014-04-23 10:04:51 -0400

    linux friendly user id lookup

--- gokud/utils.go
+++ gokud/utils.go
@@ -32,3 +32,27 @@ func lookupGroupId(group string) (gid int, err error) {
 	}
 	return 0, errors.New("group not found")
 }
+
+func lookupUserId(user string) (uid int, err error) {
+	f, err := os.Open("id -u " + user)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	br := bufio.NewReader(f)
+	for {
+		s, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+		p := strings.Split(s, ":")
+		if len(p) >= 3 && p[0] == user {
+			return strconv.Atoi(p[2])
+		}
+	}
+	return 0, errors.New("user not found")
+}
